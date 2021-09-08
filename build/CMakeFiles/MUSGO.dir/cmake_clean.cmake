b2cdada2fc93130b47db8b2275d32690c0d2249d
matheusnunesismael <nunes.matheus.ismael@gmail.com>
2021-09-08 01:00:42 -0300

    :penguin: (Compilation) Updating CMake configurations

---
+++ build/CMakeFiles/MUSGO.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MUSGO.dir/src/main.cpp.o"
+  "CMakeFiles/MUSGO.dir/src/main.cpp.o.d"
+  "MUSGO"
+  "MUSGO.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MUSGO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
